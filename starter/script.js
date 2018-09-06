9c5db0655342e7b1f89fa45b66ccf5d2a1b46540
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-06 13:14:04 +0200

    The ternary operator and switch statement

--- starter/script.js
+++ starter/script.js
@@ -184,30 +184,72 @@ GOOD LUCK 😀
 /**************************************************************************************
 * Boolean logic
 */
-var firstName = 'John';
-var age = 20;
 
-if (age < 13) {
-    console.log(firstName + ' is a boy.')
-} else if (age >= 13 && age < 20) { //between 13 and 20
-    console.log(firstName + ' is a teenager');
-} else if (age >= 20 && age < 30) {
-    console.log(firstName + ' is a young man');
-} else {
-    console.log(firstName + ' is a man.');
+// var firstName = 'John';
+// var age = 20;
+
+// if (age < 13) {
+//     console.log(firstName + ' is a boy.')
+// } else if (age >= 13 && age < 20) { //between 13 and 20
+//     console.log(firstName + ' is a teenager');
+// } else if (age >= 20 && age < 30) {
+//     console.log(firstName + ' is a young man');
+// } else {
+//     console.log(firstName + ' is a man.');
+// }
+
+
+/**************************************************************************************
+* The Ternary Operator and Switch Statements
+*/
+
+var firstName = 'John';
+var age = 16;
+
+// Ternery operator
+age >= 18 ? console.log(firstName + ' drinks beer.')
+    : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+// if (age >= 18) {
+//     var drink = 'beer'
+// } else {
+//     var drink = 'juice'
+// }
+
+// Switch statement
+var job = 'instructor';
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites');
+        break;
+    default:
+        console.log(firstName + ' does something else');
 }
 
-
-
-
-
-
-
-
-
-
-
-
+age = 18;
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}
 
 
 
