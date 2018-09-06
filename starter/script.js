baa25480ea2663a5939eaaf40039b9be08d17ca6
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-06 09:27:20 +0200

    Variable and data types

---
+++ starter/script.js
@@ -0,0 +1,32 @@
+/**************************************************************************************
+* Variable and data types
+*/
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job)
+
+// Variable naming rules
+// var _3year = 3;
+// var johnMark = 'John and Mark'
+// var if= 23;
+
+
+
+
+
+
+
+
+
