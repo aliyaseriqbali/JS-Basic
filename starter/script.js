c4c1935b1703f5e2d8e4f779b216c454e6508afb
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-06 09:29:50 +0200

    Variable mutation and type coerction

--- starter/script.js
+++ starter/script.js
@@ -23,6 +23,31 @@ console.log(job)
 // var if= 23;
 
 
+/**************************************************************************************
+* Variable mutation and type coerction
+*/
+
+var firstName = 'John';
+var age = 28;
+// Type coerction
+console.log(firstName + ' ' + age)
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '.Is he married? ' + isMarried);
+
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + ' year old ' + job + '.Is he married? ' + isMarried);
+
+
+var lastName = prompt('what is his last name?');
+console.log(firstName + ' ' + lastName);
+
 
 
 
