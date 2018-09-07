0fe2fb850da465982ff717829af4cd35ee15858d
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-07 08:28:51 +0200

    Object and methods

--- starter/script.js
+++ starter/script.js
@@ -447,30 +447,67 @@ GOOD LUCK 😀
 * Object and Properties
 */
 
-// Object literal
+// // Object literal
+// var john = {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     birthYear: 1990,
+//     family: ['Jane', 'Mark', 'Bob', 'Emily'],
+//     job: 'teacher',
+//     isMarried: false
+// }
+// console.log(john.firstName);
+// console.log(john['lastName']);
+// var x = 'birthYear';
+// console.log(john[x]);
+
+// john.job = 'designer';
+// john['isMarried'] = true;
+// console.log(john);
+
+// // New Object syntax
+// var jane = new Object();
+// jane.firstName = 'Jane';
+// jane.birthYear = 1969;
+// jane['lastName'] = 'Smith';
+// console.log(jane);
+
+
+/**************************************************************************************
+* Object and methods
+*/
+
 var john = {
     firstName: 'John',
     lastName: 'Smith',
     birthYear: 1990,
     family: ['Jane', 'Mark', 'Bob', 'Emily'],
     job: 'teacher',
-    isMarried: false
-}
-console.log(john.firstName);
-console.log(john['lastName']);
-var x = 'birthYear';
-console.log(john[x]);
+    isMarried: false,
+    calcAge: function () {
+        this.age = 2018 - this.birthYear;
+    }
+};
 
-john.job = 'designer';
-john['isMarried'] = true;
-console.log(john);
+john.calcAge();
+console.log(john)
 
-// New Object syntax
-var jane = new Object();
-jane.firstName = 'Jane';
-jane.birthYear = 1969;
-jane['lastName'] = 'Smith';
-console.log(jane);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
